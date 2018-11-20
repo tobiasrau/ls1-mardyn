6ef1e0191eef9dc27b4296243b07326714bbf945
Oliver Fernandes <oliver.fernandes@visus.uni-stuttgart.de>
2018-11-20 07:20:54 -0600

    "Fixed" cmake. Not really. CMake cannot be fixed. But a workaround is in place. Sad.

--- cmake/modules/insitu.cmake
+++ cmake/modules/insitu.cmake
@@ -10,6 +10,8 @@ message(STATUS "Installing ZMQ.")
 
     set(ZMQ_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/zeromq)
     set(ZMQ_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/libs/zeromq)
+    ## following is needed to make INTERFACE_INCLUDE_DIRECTORIES work
+    set(ZMQ_INCLUDE_DIR ${ZMQ_SOURCE_DIR}/include)
 
     # Download and install zeromq
     ExternalProject_Add(
@@ -19,21 +21,24 @@ message(STATUS "Installing ZMQ.")
         SOURCE_DIR ${ZMQ_SOURCE_DIR}
         BINARY_DIR ${ZMQ_BINARY_DIR}
         INSTALL_COMMAND ""    
-	CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     )
 
-    # Get zmq source and binary directories from CMake project
-    ExternalProject_Get_Property(zmq source_dir binary_dir)
+    # hardcore hacking to appease stupid cmake
+    # taken from https://stackoverflow.com/questions/45516209/cmake-how-to-use-interface-include-directories-with-externalproject
+    # also cf. https://www.youtube.com/watch?v=tbDw7-5701k
+    file(MAKE_DIRECTORY ${ZMQ_INCLUDE_DIR})
 
     # Create a libzmq target to be used as a dependency by the program
     add_library(libzmq IMPORTED STATIC GLOBAL)
     add_dependencies(libzmq zmq)
-
-    include_directories(
-        ${ZMQ_SOURCE_DIR}/include
+    set_target_properties(libzmq PROPERTIES
+        IMPORTED_LOCATION "${ZMQ_BINARY_DIR}/lib/libzmq.so"
+        INTERFACE_INCLUDE_DIRECTORIES "${ZMQ_INCLUDE_DIR}"
     )
-    set(ZMQ_LIB ${ZMQ_BINARY_DIR}/lib/libzmq.so)
 else()
-    set(ZMQ_LIB "")
+    # sad we have to do this, but cmake wont allow to create empty targets for linking
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/zmq_dummy.c)           # create an empty source file
+    add_library(libzmq ${CMAKE_CURRENT_BINARY_DIR}/zmq_dummy.c)   # create a dummy target from that empty file
     message(STATUS "Insitu Disabled")
 endif()
